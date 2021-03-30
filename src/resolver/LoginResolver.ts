cfc27d050c779356a80c658dfb87b0f09e4c2357
Gagah Pangeran <git@gagahpangeran.com>
2021-03-30 23:44:02 +0700

    add AuthToken type

--- src/resolver/LoginResolver.ts
+++ src/resolver/LoginResolver.ts
@@ -1,10 +1,16 @@
 import { AuthenticationError } from "apollo-server-errors";
-import { Arg, Mutation, Resolver } from "type-graphql";
+import { Arg, Field, Mutation, ObjectType, Resolver } from "type-graphql";
 import { login } from "../service/LoginService";
+
+@ObjectType()
+class AuthToken {
+  @Field({ nullable: true })
+  token?: string;
+}
 
 @Resolver()
 export default class LoginResolver {
-  @Mutation(_returns => String)
+  @Mutation(_returns => AuthToken)
   async login(
     @Arg("username") username: string,
     @Arg("password") password: string
@@ -14,6 +20,6 @@ export default class LoginResolver {
       throw new AuthenticationError("Invalid username or password!");
     }
 
-    return token;
+    return { token };
   }
 }
