64e01c4b378f092e5a5cc1b16da9b583ddd07f57
Gagah Pangeran <git@gagahpangeran.com>
2021-03-30 23:04:25 +0700

    add auth token and error in login resolver

--- src/resolver/LoginResolver.ts
+++ src/resolver/LoginResolver.ts
@@ -1,10 +1,19 @@
+import { AuthenticationError } from "apollo-server-errors";
 import { Arg, Mutation, Resolver } from "type-graphql";
 import { login } from "../service/LoginService";
 
 @Resolver()
 export default class LoginResolver {
-  @Mutation(_returns => Boolean)
-  login(@Arg("username") username: string, @Arg("password") password: string) {
-    return login(username, password);
+  @Mutation(_returns => String)
+  async login(
+    @Arg("username") username: string,
+    @Arg("password") password: string
+  ) {
+    const token = await login(username, password);
+    if (token === undefined) {
+      throw new AuthenticationError("Invalid username or password!");
+    }
+
+    return token;
   }
 }
