165f8418b731c56dda0ca3d1ba4c1be051647ff8
Gagah Pangeran <git@gagahpangeran.com>
2021-03-31 00:46:08 +0700

    add auth checker in build schema

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -1,7 +1,35 @@
-import { buildSchema } from "type-graphql";
+import { AuthenticationError } from "apollo-server-lambda";
+import jwt from "jsonwebtoken";
+import { AuthChecker, buildSchema } from "type-graphql";
 import LoginResolver from "../resolver/LoginResolver";
 import ReadListResolver from "../resolver/ReadListResolver";
+import { SECRET } from "./const";
 
 export async function createSchema() {
-  return await buildSchema({ resolvers: [LoginResolver, ReadListResolver] });
+  return await buildSchema({
+    resolvers: [LoginResolver, ReadListResolver],
+    authChecker
+  });
 }
+
+const authChecker: AuthChecker<{ bearerToken?: string }> = ({
+  context: { bearerToken }
+}) => {
+  if (bearerToken === undefined) {
+    return false;
+  }
+
+  const [type, token] = bearerToken.split(" ");
+
+  if (type !== "Bearer") {
+    throw new AuthenticationError("Invalide auth type");
+  }
+
+  try {
+    jwt.verify(token, SECRET);
+  } catch (err) {
+    throw new AuthenticationError("Invalid token");
+  }
+
+  return true;
+};
