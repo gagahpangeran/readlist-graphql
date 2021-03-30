d2dd2ccc3b370fa3c4af370cd228cc85c71f27a4
Gagah Pangeran <git@gagahpangeran.com>
2021-03-30 23:03:53 +0700

    add jwt sign in login service

--- src/service/LoginService.ts
+++ src/service/LoginService.ts
@@ -1,6 +1,10 @@
 import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import { getConnection } from "../config/db";
 import User from "../model/User";
+
+dotenv.config();
 
 async function getRepo() {
   const conn = await getConnection();
@@ -9,10 +13,14 @@ async function getRepo() {
 
 export async function login(username: string, password: string) {
   const user = await (await getRepo()).findOne({ where: { username } });
-
   if (user === undefined) {
-    return false;
+    return undefined;
   }
 
-  return await bcrypt.compare(password, user.password);
+  const checkPassword = await bcrypt.compare(password, user.password);
+  if (!checkPassword) {
+    return undefined;
+  }
+
+  return jwt.sign({ username }, process.env.SECRET ?? "secret");
 }
