8aa5f36291a57a06054beddebb9a070a11ac09f1
Gagah Pangeran <git@gagahpangeran.com>
2021-03-31 10:14:51 +0700

    add ALLOWED_HOSTS env

--- src/config/const.ts
+++ src/config/const.ts
@@ -8,7 +8,18 @@ const {
   DB_USERNAME = "user",
   DB_PASSWORD = "password",
   DB_NAME = "readlist",
-  SECRET = "secret"
+  SECRET = "secret",
+  ALLOWED_HOSTS: allowedHosts = "*"
 } = process.env;
 
-export { NODE_ENV, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, SECRET };
+const ALLOWED_HOSTS = allowedHosts.split(",");
+
+export {
+  NODE_ENV,
+  DB_HOST,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  SECRET,
+  ALLOWED_HOSTS
+};
