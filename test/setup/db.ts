ac9fbf203c88a5cc44d14bab3891c292ffd421f7
Gagah Pangeran <git@gagahpangeran.com>
2021-04-11 11:18:27 +0700

    add setup create user

--- test/setup/db.ts
+++ test/setup/db.ts
@@ -1,11 +1,12 @@
 import { createConnection } from "typeorm";
 import ReadList from "../../src/model/ReadList";
+import User from "../../src/model/User";
 
 export async function setupDB() {
   await createConnection({
     name: "test",
     type: "sqljs",
-    entities: [ReadList],
+    entities: [ReadList, User],
     synchronize: true,
     logging: false
   });
